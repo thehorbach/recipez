b2b46141faf4788ce346d4f5d7d1599d29072642
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-19 22:46:23 +0000

    Initial Commit

---
+++ recipez-v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recipez-v2
+//
+//  Created by Vyacheslav Horbach on 19/03/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
